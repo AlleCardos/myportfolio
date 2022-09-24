4bc9d6191d5c777c06a03d737b89ed3312c36bb2
AlleCardos <allecp.2002@gmail.com>
2022-09-24 14:15:47 -0300

    portfolio updates

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+//Header event
 window.addEventListener('scroll', () =>{
 
     let header = document.querySelector('.header-container');
     header.classList.toggle('sticky', window.scrollY > 0);
     
+})
+
+//Menu event
+let btn = document.querySelector('.menu-btn')
+
+btn.addEventListener('click', () =>{
+
+    let menu = document.querySelector('.header-container__navbar ul')
+
+    if(menu.classList.contains('active')){
+        menu.classList.remove('active');
+    }else{
+        menu.classList.add('active');
+    }
+
 })
