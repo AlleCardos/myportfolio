1bebac308b090ce4d7adeb69d2ced9a9d7da7c20
Allexandre Cardoso Pereira <allecp.2002@gmail.com>
2022-11-14 17:25:10 -0300

    pre -load test

---
+++ scripts/preLoad.js
@@ -0,0 +1,12 @@
+function preLoad(){
+
+    let content = document.querySelector('.portfolio-content');
+    let preContent = document.querySelector('.pre-load');
+
+    content.style.display = "block";
+    preContent.style.display = "none";
+
+}
+
+
+
